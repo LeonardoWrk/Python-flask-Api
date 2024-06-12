2ae025cedbc80b949aac780caabd35f2bf8ce7d2
leonardoWrk <leocardozo0704@gamil.com>
2024-06-12 16:25:23 -0300

    teste(sqlite): add sqlite to api unnesting code

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ from flask import Flask , request, jsonify
 from  modules.data_manager  import ENGINE, Users
 
 
+
 app = Flask(__name__)
 
 conn = ENGINE.get_connection()
 _ = Users(conn)
-ENGINE.release_connection(conn)
+
 
 @app.route("/")
 def hello_world():
@@ -14,41 +15,42 @@ def hello_world():
 
 @app.route('/database', methods=['GET'])
 def getValue():
+
      # Utilizar jsonify é uma boa prática, pois torna explícito que estou retornando um objeto JSON.
-     return jsonify(dataBase)
+     return _.addData()
 
 @app.route('/addvalue', methods=['POST'])
 def addValue():
-    conn = ENGINE.get_connection()
-    UserInterface = Users(conn)
-    # Verifica se a solicitação POST contém dados JSON
-    if request.is_json:
+        # Verifica se a solicitação POST contém dados JSON
+        if  request.is_json == False:
+            ENGINE.release_connection(conn)
+            return jsonify({'error': 'Solicitação deve conter dados JSON!'}), 400
         # Recebe os dados JSON do corpo da solicitação
         data = request.json
         print(data)
         # Verifica se a chave e o valor estão presentes nos dados recebidos
         index = 1
         for item in data:
-            if 'key' in item and 'value' in item:
-                # Adiciona a chave e o valor ao dicionário
-                minhaChave = item['key'] + str(index)  # Usando uma chave única para cada valor
-                meuValor = item['value']
-                print(minhaChave)
-                print(meuValor)
-                dataBase[minhaChave] = meuValor
-                index += 1
-                print('teste', dataBase)
-                print('teste', dataBase)
-                print('teste', dataBase) 
-                print('teste', dataBase)
-            else: 
+            print('porraaa',item)
+            minhaChave = item['key'] + str(index)  # Usando uma chave única para cada valor
+            meuValor = item['value']
+            password = 'batata'
+            if 'key' not in item and 'value' not in item:
                 ENGINE.release_connection(conn)
-                return jsonify({'error': 'Chave e/ou valor ausentes nos dados enviados!'}), 400
-        ENGINE.release_connection(conn)
-        return jsonify({'message': 'Valores adicionados com sucesso ao dicionário!'}), 200
-    else:
-        ENGINE.release_connection(conn)
-        return jsonify({'error': 'Solicitação deve conter dados JSON!'}), 400
+                return jsonify({'error': 'Chave e/ou valor ausentes nos dados enviados!'}), 400    
+        # Adiciona a chave e o valor ao dicionário
+    
+        print(minhaChave)
+        print(meuValor)
+        _.addData(minhaChave, meuValor, password)
+                            
+        index += 1
+
+                                
+                   
+                                
+                        
+
 
 @app.route('/deletevalue/<chave>', methods=['DELETE'])
 def deleteValue(chave):
@@ -60,6 +62,18 @@ def deleteValue(chave):
 
 @app.route('/updatevalue/<chave>', methods=['PATCH'])
 def updateValue(chave):
+    """
+    Update the value associated with the given key in the database.
+
+    Parameters:
+        chave (str): The key whose value needs to be updated.   
+
+    Returns:
+        JSON: A JSON response indicating the status of the operation.
+            If successful, returns a success message with HTTP status code 200.
+            If the key is not found, returns an error message with HTTP status code 404.
+            If the request body does not contain the 'value' field, returns an error message with HTTP status code 400.
+    """
     if chave in dataBase:
         # Recebe os dados JSON do corpo da solicitação
         data = request.json

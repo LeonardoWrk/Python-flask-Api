0c99248bbebaa429251b8089eacca3fd2ef9eb89
leonardoWrk <leocardozo0704@gamil.com>
2024-06-25 20:05:55 -0300

    teste(sqlite): add sqlite to api update and delete added

--- main.py
+++ main.py
@@ -1,87 +1,82 @@
 from flask import Flask , request, jsonify
 from  modules.data_manager  import ENGINE, Users
 
-
-
 app = Flask(__name__)
 
 conn = ENGINE.get_connection()
 _ = Users(conn)
-
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
 
 @app.route('/database', methods=['GET'])
-def getValue():
-
+def get_Value():
      # Utilizar jsonify é uma boa prática, pois torna explícito que estou retornando um objeto JSON.
-     return _.addData()
+     data = _.getd_data()
+
+     return data
 
 @app.route('/addvalue', methods=['POST'])
-def addValue():
-        # Verifica se a solicitação POST contém dados JSON
-        if  request.is_json == False:
+def add_value():
+    conn = ENGINE.get_connection()
+    # Verifica se a solicitação POST contém dados JSON
+    if not request.is_json:
+        ENGINE.release_connection(conn)
+        return jsonify({'error': 'Solicitação deve conter dados JSON!'}), 400
+
+    # Recebe os dados JSON do corpo da solicitação
+    data = request.json
+
+    # Verifica se a chave e o valor estão presentes nos dados recebidos
+    for item in data:
+        if 'key' not in item or 'value' not in item or 'password' not in item:
             ENGINE.release_connection(conn)
-            return jsonify({'error': 'Solicitação deve conter dados JSON!'}), 400
-        # Recebe os dados JSON do corpo da solicitação
-        data = request.json
-        print(data)
-        # Verifica se a chave e o valor estão presentes nos dados recebidos
-        index = 1
-        for item in data:
-            print('porraaa',item)
-            minhaChave = item['key'] + str(index)  # Usando uma chave única para cada valor
-            meuValor = item['value']
-            password = 'batata'
-            if 'key' not in item and 'value' not in item:
-                ENGINE.release_connection(conn)
-                return jsonify({'error': 'Chave e/ou valor ausentes nos dados enviados!'}), 400    
+            return jsonify({'error': 'Chave e/ou valor ausentes nos dados enviados!'}), 400
+    # ultimo item salvo no for anterior pode ser usado novamente: by letal
+    # [0: - 1] slice de lista descontando o ultimo item
+    for item in data:
+        if _.verify_if_exist(item['value']):
+            ENGINE.release_connection(conn)
+            return jsonify({'error': 'Valor ja existe!'}), 400
+        
+        minhaChave = item['key']  # Usando uma chave única para cada valor
+        meuValor = item['value']
+        password = item['password']
+
         # Adiciona a chave e o valor ao dicionário
-    
-        print(minhaChave)
-        print(meuValor)
-        _.addData(minhaChave, meuValor, password)
-                            
-        index += 1
+        _.add_data(minhaChave, meuValor, password)
 
-                                
-                   
-                                
-                        
-
+    ENGINE.release_connection(conn)
+    return jsonify({'message': 'Valores adicionados com sucesso ao dicionário!'}), 200
 
 @app.route('/deletevalue/<chave>', methods=['DELETE'])
-def deleteValue(chave):
-    if chave in dataBase:
-        del dataBase[chave]
-        return jsonify({'message': f'Valor com chave {chave} deletado com sucesso!'}), 200
-    else:
+def delete_Value(chave):
+    if _.verify_if_exist(chave) is False:
         return jsonify({'error': 'Chave não encontrada no dicionário!'}), 404
+        
+    _.delete_data(chave)
+    ENGINE.release_connection(conn)
+    return jsonify({'message': f'Valor com chave {chave} deletado com sucesso!'}), 200   
 
-@app.route('/updatevalue/<chave>', methods=['PATCH'])
-def updateValue(chave):
-    """
-    Update the value associated with the given key in the database.
+@app.route('/updatevalue', methods=['PATCH'])
+def update_Value():
+    data = request.json
+     
+    for item in data:
+        if 'current_email' not in item:
+            ENGINE.release_connection(conn)
+            return jsonify({'error': 'Chave e/ou valor ausentes nos dados enviados!'}), 400
+        
+    for item in data:
+        if _.verify_if_exist(item['current_email']):
+            emailatual = item['current_email']  # Define emailatual within the if block
+            name = item['nome']
+            email = item['email']
+            password = item['password']   
+            
+            _.update_data(emailatual, name, email, password)
 
-    Parameters:
-        chave (str): The key whose value needs to be updated.   
+    ENGINE.release_connection(conn)
+    return jsonify({'message': 'atualizado!'}), 200
 
-    Returns:
-        JSON: A JSON response indicating the status of the operation.
-            If successful, returns a success message with HTTP status code 200.
-            If the key is not found, returns an error message with HTTP status code 404.
-            If the request body does not contain the 'value' field, returns an error message with HTTP status code 400.
-    """
-    if chave in dataBase:
-        # Recebe os dados JSON do corpo da solicitação
-        data = request.json
-        if 'value' in data:
-            # Atualiza o valor da chave com o novo valor fornecido
-            dataBase[chave] = data['value']
-            return jsonify({'message': f'Valor com chave {chave} atualizado com sucesso!'}), 200
-        else:
-            return jsonify({'error': 'O campo "value" é obrigatório nos dados enviados!'}), 400
-    else:
-        return jsonify({'error': 'Chave não encontrada no dicionário!'}), 404
--- modules/data_manager.py
+++ modules/data_manager.py
@@ -19,15 +19,73 @@ class Users:
             )'''
         )
 
-    def addData(self, name:str, email:str, password:str):
+    def add_data(self, name:str, email:str, password:str):
 
         cur = self.connection.cursor()
         sql_update_query = """INSERT INTO Users (
             Name,
             Email,
-            PassWord 
+            PassWord
         ) VALUES (?, ?, ?)"""
         cur.execute(sql_update_query, (name, email, password,))
         self.connection.commit()
 
         return
+
+
+    def getd_data(self):
+
+        cur = self.connection.cursor()
+        sql_get_query = """SELECT * FROM Users"""
+        cur.execute(sql_get_query)
+        rows = cur.fetchall()
+
+        return rows
+
+    def verify_if_exist(self, name:str) -> bool:
+
+        cur = self.connection.cursor()
+        sql_check_query = """SELECT EXISTS(SELECT 1 FROM Users WHERE Email = ?)"""
+        cur.execute(sql_check_query, (name,))
+        result = cur.fetchone()[0]
+        
+
+        return result == 1
+
+
+    def delete_data(self, name:str) -> bool:
+
+        cur = self.connection.cursor()  
+        sql_delete_query = """DELETE FROM Users WHERE Email = ?"""
+        cur.execute(sql_delete_query, (name,))
+        self.connection.commit()
+        affected_rows = cur.rowcount
+
+        return affected_rows > 0
+    
+
+    def update_data(self, current_email: str, name: str = None, email: str = None, password: str = None):
+        cur = self.connection.cursor()
+        sql_update_query = "UPDATE Users SET "
+        params = []
+
+        if name is not None:
+            #+= para concatenar strings ou adicionar valores a variáveis numéricas.
+            sql_update_query += "Name = ?, "
+            params.append(name)
+        if email is not None:
+            sql_update_query += "Email = ?, "
+            params.append(email)
+        if password is not None:
+            sql_update_query += "PassWord = ?, "
+            params.append(password)
+        
+        # Remove a vírgula extra no final e adiciona a cláusula WHERE
+        sql_update_query = sql_update_query.rstrip(', ') + " WHERE Email = ?"
+        params.append(current_email)
+        
+        cur.execute(sql_update_query, tuple(params))
+        self.connection.commit()
+        affected_rows = cur.rowcount
+
+        return affected_rows > 0
